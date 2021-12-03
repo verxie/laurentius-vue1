eb19963dc2d72cde198dfd28c749a47386f0c82e
Laurentius <laurentius@mhs.mdp.ac.id>
2021-12-03 19:16:13 +0700

    New

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="row">
+        <div class="col-md-6 my-4">
+            <div class="animation1"></div>
+        </div>
+
+        <div class="col-md-6 my-5 text-dark animation2">
+            <h1 class="fw-bolder">Introduction</h1>
+            <p class="h6 fw-bold">Hey! My name is Laurentius. I am 18 years old. I graduated from Xaverius Vocational Highschool and a student at Multi Data Palembang University and I'm also working at a company that produce and sells hospital and rehabilitation product. About my hobbies, I don't have much hobbies, but I'm sure you can tell that I watch anime...from the picture on the left, I also watch YouTube videos or playing video game like Valorant</p>
+            <hr>
+            <h1 class="fw-bolder">Internship</h1>
+            <p class="h6 fw-bold">
+                <ul>
+                    <li>
+                        November 2019 - February 2019 at Artha Komputer
+                    </li>
+
+                    <li>
+                        July 2020 - October 2020 at Chandra Komputer
+                    </li>
+                </ul>
+            </p>
+            <hr>
+            <h1 class="fw-bolder">Achievement</h1>
+            <p class="h6 fw-bold">
+                <ul>
+                    <li>800 out of 990 TOEIC score</li>
+                </ul>
+            </p>
+        </div>
+    </div>
   </div>
 </template>
 
